7c0cdc2e8ccd2c67d96974dcb0337b1bd02c85fb
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2026-05-04 23:55:10 +0100

    Refactor formatting in yearlyProfitCalculator function for consistency

--- src/profits.js
+++ src/profits.js
@@ -1,6 +1,6 @@
 export function yearlyProfitCalculator(startAmount, interestRate) {
-    const tax = 18;
-    const profit =
-        startAmount * ((100 + interestRate) / 100 - 1) * ((100 - tax) / 100);
-    return profit;
+  const tax = 18;
+  const profit =
+    startAmount * ((100 + interestRate) / 100 - 1) * ((100 - tax) / 100);
+  return profit;
 }
