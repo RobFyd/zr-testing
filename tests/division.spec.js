4164481b915cb8b6d64f6b8dd2ca0378741a4947
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2026-05-05 00:09:56 +0100

    Add initial test for division function

---
+++ tests/division.spec.js
@@ -0,0 +1,13 @@
+import { division } from "../src/division.js";
+
+function test1() {
+  const a = 42;
+  const b = 6;
+  const expectedResult = 7;
+
+  const actualResult = division(a, b);
+
+  if (actualResult === expectedResult) {
+    console.log("Test 1 passed");
+  }
+}
