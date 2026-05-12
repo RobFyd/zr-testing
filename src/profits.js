4a01256a5f0ff32b460a9f15f65f0c3909df7c7c
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2026-05-13 00:21:05 +0100

    Update tax rate in yearlyProfitCalculator and ensure profit is returned as a fixed decimal

--- src/profits.js
+++ src/profits.js
@@ -1,6 +1,6 @@
 export function yearlyProfitCalculator(startAmount, interestRate) {
-  const tax = 18;
+  const tax = 19;
   const profit =
     startAmount * ((100 + interestRate) / 100 - 1) * ((100 - tax) / 100);
-  return profit;
+  return parseFloat(profit.toFixed(2));
 }
